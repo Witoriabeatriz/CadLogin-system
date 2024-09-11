f3ee7bc211203f1f28d7fda9eb9ce781947872f8
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-09-11 09:07:52 -0300

    Instanciar os controladores

--- routes.php
+++ routes.php
@@ -1,6 +1,13 @@
 <?php
-//inclui arquivos de controlador necessarios para lidar com diferentes ações
+//Inclui arquivos de controlador necessarios para lidar com diferentes ações
 require 'controllers/AuthController.php'; //inclui o controlador de autenticação
 require 'controllers/UserController.php'; //inclui o controlador de usuários
 require 'controllers/DashboardController.php'; //inclui o controlador de dashboard
+
+// Cria instâncias dos controladores para utilizar seus metodos
+$authController = new AuthController(); // Instancia controlador de autenticação
+$userController = new UserController(); // Instancia controlador de usuário
+$dashboardController = new DashboardController(); // Instancia controlador de dashboard
+
+
 ?>

2c90aab67ea856392efd49197ef2a832006e0045
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-09-11 08:56:40 -0300

    Inclui arquivos de controlador

---
+++ routes.php
@@ -0,0 +1,6 @@
+<?php
+//inclui arquivos de controlador necessarios para lidar com diferentes ações
+require 'controllers/AuthController.php'; //inclui o controlador de autenticação
+require 'controllers/UserController.php'; //inclui o controlador de usuários
+require 'controllers/DashboardController.php'; //inclui o controlador de dashboard
+?>

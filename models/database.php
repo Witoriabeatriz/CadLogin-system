9ca026c9cc067984532b06344f175cb2275d7e50
Witoriabeatriz <witoria.santos@alunoescolassociais.org.br>
2024-11-13 07:39:03 -0300

    atualiz models

--- models/database.php
+++ models/database.php
@@ -1,25 +1,28 @@
 <?php
-// Define classe Database, responsável por gerenciar conexão com banco de dados
-class Database{
-    // Padrão singleton, cujo objetivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja reutilizada sempre que necessário
-    private static $instance = null;
-    public static function getConnection(){
-        if(!self::$instance){
-            //configuração de conexão com BD
-            $host        = 'localhost';
-            $db          = 'sistema_usuarios';
-            $user        = 'root';
-            $password    = '';
 
-            // A conexão usa o driver Mysqli (mysql:) e as informações de host e BD
-            self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-           // Define o modo de erro para exeções, facilitando a depuração e tratamento de erro
+// Define classe Database, responsável por gerenciar a conexão com o banco de dados
+class Database
+{
+    // Padrão Singleton, cujo objetivo é garantir que apenas uma única instância de classe seja criada durante a execução do programa, e que essa instância seja reutiliazda sempre que necessário
+    private static $instance = null;
+
+    public static function getConnection() {
+        if (!self::$instance) {
+            // Configurações de conexão com o BD
+            $host = "localhost";
+            $db   = "sistema_usuarios";
+            $user = "root";
+            $pass = "";
+
+            // A conexão usa o driver MySQL (mysql:) e as informações de host e BD
+            self::$instance = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+            
+            // Define o modo de erro para exceções, facilitando a depuração e tratamento do erro
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+
         return self::$instance;
     }
 }
-
-
 
 ?>
--- models/user.php
+++ models/user.php
@@ -8,22 +8,22 @@ class User
     public static function findByEmail($email) {
         // Obter conexão com o banco de dados
         $conn = Database::getConnection();
-
+        
         // Prepara consulta SQL para buscar usuário pelo email
         $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
         $stmt->execute([":email" => $email]);
-
+        
         // Retorno de dados do usuário encontrado como um array associativo
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
     // Cria função que localiza usuário pelo ID
-    public static function find($ID) {
+    public static function find($id) {
         // Obtém a conexão com o banco de dados
         $conn = Database::getConnection();
-
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE ID = :ID");
-        $stmt->execute(["ID" => $ID]);
+        
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $stmt->execute(["id" => $id]);
 
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
@@ -34,6 +34,7 @@ class User
         $stmt = $conn->prepare("INSERT INTO usuarios(nome, email, senha, perfil) VALUES (:nome, :email, :senha, :perfil)");
         $stmt->execute($data);
     }
+
     // Função para listar todas as informações dos usuários no BD
     public static function all() {
         $conn = Database::getConnection();
@@ -42,20 +43,22 @@ class User
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function update($ID, $data){
+    public static function update($id, $data) {
         $conn = Database::getConnection();
-        // Prepara a consulta sql para autorizaçâo do dados do usuário
-        $stmt = $conn->prepare("UPDATE usuarios SET nome = :nome, email = :email, perfil = :perfil WHERE ID = :ID");
-        $data['ID'] = $ID;
+
+        // Prepara a consulta SQL para atualização dos dados do usuário
+        $stmt = $conn->prepare("UPDATE usuarios SET nome = :nome, email = :email, perfil = :perfil WHERE id = :id");
+
+        $data["id"] = $id;
+
         $stmt->execute($data);
     }
 
-    //FUNÇÃO PARA EXCLUSÃO DE UM USUÁRIO PELO ID
-    public static function delete($ID)
-    {
+    // Função para exclusão de usuário pelo ID
+    public static function delete($id) {
         $conn = Database::getConnection();
-        $stmt = $conn->prepare('DELETE FROM usuarios WHERE ID= :ID');
-        $stmt->execute(['ID' => $ID]);
+        $stmt = $conn->prepare("DELETE FROM usuarios WHERE id = :id");
+        $stmt->execute(["id" => $id]);
     }
 }
 
